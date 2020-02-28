43eb23afe0098689c16cb7515adc5122ee4cd7d1
akash <akash.rastogi@techment.com>
2020-02-29 00:23:41 +0530

    commit 2 new component to show <ul><li> minor rename of elements and added few more information rename the enrty element's id

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,25 @@
 import * as React from "react";
-import * as ReactDom from "react-dom";
+import * as ReactDOM from "react-dom";
 
-import NamasteComponent from "./components/namaste";
+//  import { NamasteComponent } from "./components/namaste";
+import { List } from "./components/list";
 
-ReactDom.render(<NamasteComponent/>,
+/***
+ * This is the Entry Point
+ * Adjacent JSX elements must be wrapped in an enclosing tag
+ *  Ex.below code is not valid
+  **  ReactDOM.render(<h1>1</h1><h2>2</h2>,
       document.getElementById('main'))
+  **  ReactDOM.render(<div>
+      <h1>1</h1>
+      <h2>2</h2>
+    </div>
+    , document.getElementById('main'))
+    is valid
+ */
+ReactDOM.render(
+  <div>
+    <List />
+  </div>,
+  document.getElementById("app-react")
+);
---
+++ src/components/list.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export class List extends React.Component {
+    render(){
+        return(
+            <ul>
+                <li>A</li>
+                <li>B</li>
+                <li>C</li>
+            </ul>
+        )
+    }
+}
