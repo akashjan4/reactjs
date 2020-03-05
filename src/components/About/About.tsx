f2b90e0d922d5a37e0f5cbdb3ca4020c6d9cb230
akash <akash.rastogi@techment.com>
2020-03-05 09:57:32 +0530

    commit 10 use of Router to create single page application

---
+++ src/components/About/About.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+export class About extends React.Component {
+  render() {
+    return <h1> Hi I\'m home </h1>;
+  }
+}
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,17 +1,28 @@
 import * as React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { MainContent } from "./mainContent";
 import { NavigationBar } from "./navigationBar";
+import { About } from "./About/About";
+import { Services } from "./Services/Service";
 import { SideNavigation } from "./sideNavigation/sideNavigation";
 
 export class App extends React.Component {
-  navigationLinks  = [{text:'About', link:'home'}]
+  navigationLinks = [{ text: "About", link: "home" }];
   render() {
     return (
-      <div className="parent-container">
-        {/* <NavigationBar/>
+      <Router>
+        <div className="parent-container">
+          {/* <NavigationBar/>
         <MainContent /> */}
-        <SideNavigation/>
-      </div>
+          {/* <SideNavigation /> */}
+          <Route exact path="/">
+            <About />
+          </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
+        </div>
+      </Router>
     );
   }
 }
